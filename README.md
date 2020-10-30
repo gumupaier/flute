3e3c82d8d9005744975066919bd4e13259550e5d
henson <henson_wu@foxmail.com>
2020-10-30 20:20:39 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # flute
+[![GitHub issues](https://img.shields.io/github/issues/gumupaier/flute)](https://github.com/gumupaier/flute/issues) [![GitHub forks](https://img.shields.io/github/forks/gumupaier/flute)](https://github.com/gumupaier/flute/network) [![GitHub stars](https://img.shields.io/github/stars/gumupaier/flute)](https://github.com/gumupaier/flute/stargazers) [![GitHub license](https://img.shields.io/github/license/gumupaier/flute)](https://github.com/gumupaier/flute/blob/main/LICENSE)
+
 nebula graph database toolkit python version
+
---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/30 8:07 下午
+# @Author  : Henson
+# @Email   : henson_wu@foxmail.com
+# @File    : setup.py
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+install_packages = []
+setup(
+    name='flute',
+    version='1.0',
+    description='flute',
+    long_description=readme,
+    author='henson',
+    author_email='henson_wu@foxmail.com',
+    url='',
+    packages=find_packages(exclude=('tests', 'docs', 'templates')),
+    package_data={
+        "flute": ["*.jar"]
+    },
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
